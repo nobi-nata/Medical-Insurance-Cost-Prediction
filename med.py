04a1cd1917b7db020303f207373f87d21796f0f8
Laxmikant <85544667+nobi-nata@users.noreply.github.com>
2022-02-18 18:38:48 +0530

    ninth commit

--- med.py
+++ med.py
@@ -1,12 +1,8 @@
 import streamlit as st
 import pandas as pd 
 from matplotlib import pyplot as plt
-from plotly import graph_objs as go
 import numpy as np 
 import time
-# import seaborn as plt
-from sklearn.model_selection import train_test_split
-
 from sklearn.linear_model import LinearRegression
 
 
@@ -17,108 +13,260 @@ md_df =pd.read_csv("medical report.csv")
 st.title("Medical Insurance Cost Predictor")
 st.image("https://miro.medium.com/max/700/1*WoVYSG5nZbErqzgy47tWBQ.jpeg",width = 450)
 nav = st.sidebar.radio("Navigation",["Home","Prediction","Contribute"])
+
+from sklearn.preprocessing import LabelEncoder
+le = LabelEncoder()
+label2 = le.fit_transform(md_df['blood sugar'])
+md_df.drop("blood sugar",axis=1,inplace=True)
+md_df["blood sugar"]=label2
+
+
 if nav == "Home":
     st.header("Know Our Dataset")
-    if st.checkbox("Show Dataset"):
-        st.dataframe(data)
     
-    graph = st.selectbox("Select",["Numeric","Graphic"])
-    value = st.selectbox("Select Column",["Age","Gender","BMI","Children","Medical-Report","Region","Charges"])
-    if graph == "Numeric":
-        if value == "Age":
-            a = data['age'].value_counts()
-            st.table(a)
-        elif value == "Gender":
-            b = data['gender'].value_counts()
-            st.table(b)
-        elif value == "BMI":
-            c = data['bmi'].value_counts()
-            st.table(c)
-        elif value == "Children":
-            d = data['children'].value_counts()
-            st.table(d)
-        elif value == "Medical":
-            e = data['medical'].value_counts()
-            st.table(e)
-        elif value == "Region":
-            f = data['region'].value_counts()
-            st.table(f)
-        else:
-            g = data['charges']
-            st.table(g)
-    if graph == "Graphic":
-        if value == "Age":
-            # Distribution of age value
-            plt.figure(figsize=(6,6))
-            arr = data['age']
-            fig, ax = plt.subplots()
-            ax.hist(arr, bins="fd")
-            plt.title('Age Distribution')
-            plt.xlabel('Age')
-            plt.ylabel('Density')
-            st.pyplot(fig)        
-        elif value == "Gender":
-            # Gender column
-            plt.figure(figsize=(6,6))
-            arr = data['gender']
-            fig, ax = plt.subplots()
-            ax.hist(arr, bins='scott',color='green')
-            plt.title('Gender Distribution')
-            plt.xlabel('Gender')
-            plt.ylabel('Density')
-            st.pyplot(fig) 
-            
-        elif value == "BMI":
-            # BMI distribution
-            # Normal BMI range --> 18.5 to 24.9
-            plt.figure(figsize=(6,6))
-            arr = data['bmi']
-            fig, ax = plt.subplots()
-            ax.hist(arr, bins=20,color='grey')
-            plt.title('BMI Distribution')
-            plt.xlabel('BMI')
-            plt.ylabel('Density')
-            st.pyplot(fig) 
-        elif value == "Children":
-            # Childern column
-            plt.figure(figsize=(6,6))
-            arr = data['children']
-            fig, ax = plt.subplots()
-            ax.hist(arr, bins='sturges',color='orange')
-            plt.title('Children Distribution')
-            plt.xlabel('Children')
-            plt.ylabel('Density')
-            st.pyplot(fig) 
-        elif value == "Medical-Report":
-            # Medical-Report column
-            plt.figure(figsize=(6,6))
-            arr = data['medical']
-            fig, ax = plt.subplots()
-            ax.hist(arr, bins='scott',color='purple')
-            plt.title('Medical-Report Distribution')
-            plt.xlabel('Medical-Report')
-            plt.ylabel('Density')
-            st.pyplot(fig) 
-        elif value == "Region":
-            # Region distribution
-            plt.figure(figsize=(6,6))
-            arr = data['region']
-            fig, ax = plt.subplots()
-            ax.hist(arr, bins='scott')
-            plt.title('Region Distribution')
-            plt.xlabel('Region')
-            plt.ylabel('Density')
-            st.pyplot(fig) 
-        else:
-            # Distribution of charge value
-            plt.figure(figsize=(6,6))
-            arr = data['charges']
-            fig, ax = plt.subplots()
-            ax.hist(arr,bins=[2500,5000,7500,10000,12500,15000,17500,20000,22500,25000,27500,30000,32500,35000,37500,40000,42500,45000,47500,50000])
-            plt.title('Price Distribution')
-            plt.xlabel('Charges')
-            plt.ylabel('Density')
-            st.pyplot(fig)
+    datset = st.selectbox("Select the Dataset",["Medical-Insurance","Medical-Report"])
+
+    if datset == "Medical-Insurance":
+        if st.checkbox("Show Dataset"):
+            st.dataframe(data)
+        graph = st.selectbox("Select Type",["Numeric","Graphic"])
+        value = st.selectbox("Select Column",["Age","Gender","BMI","Children","Medical-Report","Region","Charges"])
+        if graph == "Numeric":
+            if value == "Age":
+                a = data['age'].value_counts()
+                st.table(a)
+            elif value == "Gender":
+                b = data['gender'].value_counts()
+                st.table(b)
+            elif value == "BMI":
+                c = data['bmi'].value_counts()
+                st.table(c)
+            elif value == "Children":
+                d = data['children'].value_counts()
+                st.table(d)
+            elif value == "Medical":
+                e = data['medical'].value_counts()
+                st.table(e)
+            elif value == "Region":
+                f = data['region'].value_counts()
+                st.table(f)
+            else:
+                g = data['charges']
+                st.table(g)
+        if graph == "Graphic":
+            if value == "Age":
+                # Distribution of age value
+                plt.figure(figsize=(6,6))
+                arr = data['age']
+                fig, ax = plt.subplots()
+                ax.hist(arr, bins="fd")
+                plt.title('Age Distribution')
+                plt.xlabel('Age')
+                plt.ylabel('Density')
+                st.pyplot(fig)        
+            elif value == "Gender":
+                # Gender column
+                plt.figure(figsize=(6,6))
+                arr = data['gender']
+                fig, ax = plt.subplots()
+                ax.hist(arr, bins='scott',color='green')
+                plt.title('Gender Distribution')
+                plt.xlabel('Gender')
+                plt.ylabel('Density')
+                st.pyplot(fig) 
+                
+            elif value == "BMI":
+                # BMI distribution
+                # Normal BMI range --> 18.5 to 24.9
+                plt.figure(figsize=(6,6))
+                arr = data['bmi']
+                fig, ax = plt.subplots()
+                ax.hist(arr, bins=20,color='grey')
+                plt.title('BMI Distribution')
+                plt.xlabel('BMI')
+                plt.ylabel('Density')
+                st.pyplot(fig) 
+            elif value == "Children":
+                # Childern column
+                plt.figure(figsize=(6,6))
+                arr = data['children']
+                fig, ax = plt.subplots()
+                ax.hist(arr, bins='sturges',color='orange')
+                plt.title('Children Distribution')
+                plt.xlabel('Children')
+                plt.ylabel('Density')
+                st.pyplot(fig) 
+            elif value == "Medical-Report":
+                # Medical-Report column
+                plt.figure(figsize=(6,6))
+                arr = data['medical']
+                fig, ax = plt.subplots()
+                ax.hist(arr, bins='scott',color='purple')
+                plt.title('Medical-Report Distribution')
+                plt.xlabel('Medical-Report')
+                plt.ylabel('Density')
+                st.pyplot(fig) 
+            elif value == "Region":
+                # Region distribution
+                plt.figure(figsize=(6,6))
+                arr = data['region']
+                fig, ax = plt.subplots()
+                ax.hist(arr, bins='scott')
+                plt.title('Region Distribution')
+                plt.xlabel('Region')
+                plt.ylabel('Density')
+                st.pyplot(fig) 
+            else:
+                # Distribution of charge value
+                plt.figure(figsize=(6,6))
+                arr = data['charges']
+                fig, ax = plt.subplots()
+                ax.hist(arr,bins=[2500,5000,7500,10000,12500,15000,17500,20000,22500,25000,27500,30000,32500,35000,37500,40000,42500,45000,47500,50000])
+                plt.title('Price Distribution')
+                plt.xlabel('Charges')
+                plt.ylabel('Density')
+                st.pyplot(fig)
+
+
+    if datset == "Medical-Report":
+        if st.checkbox("Show Dataset"):
+            st.dataframe(md_df)
+        graph = st.selectbox("Select Type",["Numeric","Graphic"])
+        value = st.selectbox("Select Column",["Age","Gender","Number of Surgeries","Blood Pressure","Colestrol","Blood Sugar","ECG","Heart Rate","Bone Fracture","Target"])
+        if graph == "Numeric":
+            if value == "Age":
+                h = md_df['age'].value_counts()
+                st.table(h)
+            elif value == "Gender":
+                i = md_df['gender'].value_counts()
+                st.table(i)
+            elif value == "Number of Surgeries":
+                j = md_df['no. of surgeries'].value_counts()
+                st.table(j)
+            elif value == "Blood Pressure":
+                k = md_df['blood pressure'].value_counts()
+                st.table(k)
+            elif value == "Colestrol":
+                l = md_df['cholestrol'].value_counts()
+                st.table(l)
+            elif value == "Blood Sugar":
+                m = md_df['blood sugar'].value_counts()
+                st.table(m)
+            elif value == "ECG":
+                n = md_df['ecg'].value_counts()
+                st.table(n)
+            elif value == "Heart Rate":
+                o = md_df['max. heart rate'].value_counts()
+                st.table(o)
+            elif value == "Bone Fracture":
+                p = md_df['bone fracture'].value_counts()
+                st.table(p)
+            else:
+                q = md_df['Target'].value_counts()
+                st.table(q)
+        if graph == "Graphic":
+            if value == "Age":
+                # Distribution of age value
+                plt.figure(figsize=(6,6))
+                arr = md_df['age']
+                fig, ax = plt.subplots()
+                ax.hist(arr, bins="fd")
+                plt.title('Age Distribution')
+                plt.xlabel('Age')
+                plt.ylabel('Density')
+                st.pyplot(fig)        
+            elif value == "Gender":
+                # Gender column
+                plt.figure(figsize=(6,6))
+                arr = md_df['gender']
+                fig, ax = plt.subplots()
+                ax.hist(arr, bins='scott',color='green')
+                plt.title('Gender Distribution')
+                plt.xlabel('Gender')
+                plt.ylabel('Density')
+                st.pyplot(fig) 
+                
+            elif value =="Number of Surgeries":
+                # surgeries distribution
+                # Normal BMI range --> 18.5 to 24.9
+                plt.figure(figsize=(6,6))
+                arr = md_df['no. of surgeries']
+                fig, ax = plt.subplots()
+                ax.hist(arr, bins=20,color='grey')
+                plt.title('Surgeries Distribution')
+                plt.xlabel('Surgeries')
+                plt.ylabel('Density')
+                st.pyplot(fig) 
+            elif value == "Blood Pressure":
+                # Blood Pressure column
+                plt.figure(figsize=(6,6))
+                arr = md_df['blood pressure']
+                fig, ax = plt.subplots()
+                ax.hist(arr, bins='sturges',color='orange')
+                plt.title('Blood Pressure Distribution')
+                plt.xlabel('Blood Pressure')
+                plt.ylabel('Density')
+                st.pyplot(fig) 
+            elif value == "Cholestrol":
+                # Colestrol column
+                plt.figure(figsize=(6,6))
+                arr = md_df['cholestrol']
+                fig, ax = plt.subplots()
+                ax.hist(arr, bins='scott',color='purple')
+                plt.title('Cholestrol Distribution')
+                plt.xlabel('Cholestrol')
+                plt.ylabel('Density')
+                st.pyplot(fig) 
+            elif value == "Blood Sugar":
+                # Blood Sugar distribution
+                plt.figure(figsize=(6,6))
+                arr = md_df['blood sugar']
+                fig, ax = plt.subplots()
+                ax.hist(arr, bins='scott')
+                plt.title('Blood Sugar Distribution')
+                plt.xlabel('Blood Sugar')
+                plt.ylabel('Density')
+                st.pyplot(fig) 
+            elif value == "ECG":
+                # ECG distribution
+                plt.figure(figsize=(6,6))
+                arr = md_df['ecg']
+                fig, ax = plt.subplots()
+                ax.hist(arr, bins='scott')
+                plt.title('ECG Distribution')
+                plt.xlabel('ECG')
+                plt.ylabel('Density')
+                st.pyplot(fig) 
+            elif value == "Heart Rate":
+                # Heart Rate distribution
+                plt.figure(figsize=(6,6))
+                arr = md_df['max. heart rate']
+                fig, ax = plt.subplots()
+                ax.hist(arr, bins='scott')
+                plt.title('Heart Rate Distribution')
+                plt.xlabel('Heart Rate')
+                plt.ylabel('Density')
+                st.pyplot(fig) 
+            elif value == "Bone Fracture":
+                # Region distribution
+                plt.figure(figsize=(6,6))
+                arr = md_df['bone fracture']
+                fig, ax = plt.subplots()
+                ax.hist(arr, bins='scott')
+                plt.title('Bone Fracture Distribution')
+                plt.xlabel('Bone Fracture')
+                plt.ylabel('Density')
+                st.pyplot(fig) 
+            else:
+                # Distribution of charge value
+                plt.figure(figsize=(6,6))
+                arr = md_df['Target']
+                fig, ax = plt.subplots()
+                ax.hist(arr,bins="fd")
+                plt.title('Target Distribution')
+                plt.xlabel('Target')
+                plt.ylabel('Density')
+                st.pyplot(fig)
 
 data['gender']=data['gender'].map({'female':0,'male':1})
 data['medical']=data['medical'].map({'yes':1,'no':0})
@@ -162,11 +310,11 @@ if nav == "Prediction":
 
     st.subheader("Enter your Medical-details")
     sur = st.number_input("Number of Surgeries",0.0,10.0,step=1.0)
-    bp = st.number_input("Blood Pressure",50.0,200.0,step=0.5)
-    col = st.number_input("Colestrol",100.0,300.0,step=1.0)
+    bp = st.number_input("Blood Pressure",90,200.0,step=0.5)
+    col = st.number_input("Colestrol",100.0,350.0,step=1.0)
     bs = st.radio("Blood Sugar",["no","yes"])
     ecg = st.radio("ECG",["no","yes"])
-    mhr = st.number_input("Max. Heart Rate",100.0,200.0,step=1.0)
+    mhr = st.number_input("Max. Heart Rate",30,200.0,step=1.0)
     bf = st.radio("Bone Fracture",["no","yes"])
     
     if gen == 'male':
@@ -207,11 +355,11 @@ if nav == "Prediction":
     # reshape the array
     input_data_reshaped1 = input_data_as_numpyArray1.reshape(1,-1)
     med = LR.predict(input_data_reshaped1)
-    if st.button("Check"):
-        if med == 0:
-            st.success("You are Medically Unfit")
-        else:
-            st.success("You are Medically Fit")
+    # if st.button("Check"):
+    #     if med == 0:
+    #         st.success("You are Medically Unfit")
+    #     else:
+    #         st.success("You are Medically Fit")
 
     if med == 1:
         medi = 1
